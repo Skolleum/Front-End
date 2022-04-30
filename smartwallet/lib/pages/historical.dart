ef628ab2538d8aa82afb3f57a8c6082e40dcf426
lowkeyboard <caqlarybak@gmail.com>
2022-04-30 15:56:00 -0700

    bottom navigation added

---
+++ smartwallet/lib/pages/historical.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class HistoricalPage extends StatelessWidget {
+  const HistoricalPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Text("Historicalpage"),
+    );
+  }
+}
